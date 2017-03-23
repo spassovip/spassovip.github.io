ea05ec1221f7e1c3470eac529e1c5607d144dd2a
Gabrielbdd <gabrielberto@icloud.com>
2017-03-23 13:49:03 -0300

    Site updated: 2017-03-23 13:49:03

--- js/app.js
+++ js/app.js
@@ -39,3 +39,10 @@ var appDaily = {
 };
 
 appDaily.bindToggleButton();
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://spassovip.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
